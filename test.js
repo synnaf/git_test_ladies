2af1a79e6910389ec604d6779701201312ee3cb2
Fanny <32634522+synnaf@users.noreply.github.com>
2020-01-21 14:41:26 +0100

    en app

--- test.js
+++ test.js
@@ -1,5 +1,18 @@
+const express = require("express"); 
+const app = express(); 
+
+app.use(express.json()); 
+
+
+app.get("/git_app", (req, res)=> {
+    res.send("Welcome to da APP"); 
+});
+
+app.listen(8060); 
+
 console.log("Hej hej 8-)"); 
 
 console.log("Text från Isabelle");
 
-console.log("Tjenare från Gittan"); 
+console.log("Tjenare från Gittan");
+
