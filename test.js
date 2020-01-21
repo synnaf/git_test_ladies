fff0a393de59d300dc8823e3f0f9ef23e42b125f
[isabelleedlund] <[edlund.isabelle@gmail.com]>
2020-01-21 13:52:29 +0100

    Space

--- test.js
+++ test.js
@@ -1,8 +1,5 @@
 console.log("Hej hej 8-)"); 
 
-<<<<<<< HEAD
 console.log("Text från Isabelle");
-=======
 
-console.log("Tjenare från Gittan");
->>>>>>> b1eeeebbb02a9f290309286c572743cae95ff055
+console.log("Tjenare från Gittan"); 
